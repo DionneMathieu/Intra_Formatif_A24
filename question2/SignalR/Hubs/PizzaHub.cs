74784e3b35a1b7610deb3bc923606d7982c58cc1
DionneMathieu <167901916+DionneMathieu@users.noreply.github.com>
2025-10-03 14:49:56 -0400

    wip Question 2

--- question2/SignalR/Hubs/PizzaHub.cs
+++ question2/SignalR/Hubs/PizzaHub.cs
@@ -14,15 +14,50 @@ namespace SignalR.Hubs
         public override async Task OnConnectedAsync()
         {
             await base.OnConnectedAsync();
+            //Ajoute l'utilisateur
+            _pizzaManager.AddUser();
+
+            //Mise a jour du nombre d'utilisateur
+            await Clients.All.SendAsync("UpdateNbUsers ", _pizzaManager.NbConnectedUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             await base.OnConnectedAsync();
+            //Retire l'utilisateur
+            _pizzaManager.RemoveUser();
+
+            //Mise a jour du nombre d'utilisateur
+            await Clients.All.SendAsync("UpdateNbUsers ", _pizzaManager.NbConnectedUsers);
         }
 
         public async Task SelectChoice(PizzaChoice choice)
         {
+            //Groupe 1 = choix de la pizza selon le nom
+            string Choix = _pizzaManager.GetGroupName(choice);
+            
+            /*
+            int money = _pizzaManager.Money[(int)choice];
+            int pizzaPrice = _pizzaManager.PIZZA_PRICES[(int)choice];
+            int nbPizzas = _pizzaManager.NbPizzas[(int)choice];
+
+            int[] nbPizzaAndMoney = [money, nbPizzas];
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, Choix);
+
+            await Clients.Group(Choix).SendAsync("UpdateNbPizzasAndMoney", nbPizzaAndMoney);
+            await Clients.All.SendAsync("UpdatePizzaPrice", pizzaPrice);
+            */
+
+            
+             int[] nbPizzaAndMoney = [_pizzaManager.Money[(int)choice], _pizzaManager.NbPizzas[(int)choice]];
+              
+            await Groups.AddToGroupAsync(Context.ConnectionId, _pizzaManager.GetGroupName(choice));
+            
+            await Clients.Group(_pizzaManager.GetGroupName(choice)).SendAsync("UpdateNbPizzasAndMoney", nbPizzaAndMoney)
+
+             
+
         }
 
         public async Task UnselectChoice(PizzaChoice choice)
